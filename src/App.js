747f46832410383217abceae404fb173b4a31ffb
Emeka Ugbanu <jemeka865@gmail.com>
2022-04-28 10:48:17 -0900

    make it better

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const PDP = lazy(() => import("./pages/ProductDisplay/pdpgraphql"));
 const Header = lazy(() => import("./components/Header/header"));
 const Cart = lazy(() => import("./pages/Cart/Cart"));
 
-const link = from([new HttpLink({ uri: "http://localhost:4000" })]);
+const link = from([new HttpLink({ uri:  process.env.REACT_APP_GRAPHQL_ENDPOINT || "http://localhost:4000" })]);
 
 const client = new ApolloClient({
   cache: new InMemoryCache({
